315e4b6ff0f2bb531a8895ad56d2965c2dba99f2
John Marcus III <j3@jwmarcus.com>
2024-03-16 20:11:06 -0400

    initial components

--- src/App.js
+++ src/App.js
@@ -4,36 +4,11 @@ import './App.css';
 import React, { useState } from 'react';
 // import { click } from '@testing-library/user-event/dist/click';
 
-function MyButton(props)  {
-  // Initialize teh state variable'clickCount' to 0
-  // NOTE: This is the standard pattern for initializing state in a function component
-  
-  const [clickCount, setClickCount] = useState(0);
-
-  const handleClick = () => {
-    // Increment click count
-    setClickCount(clickCount + 1);
-
-    // Call the onClick prop if it is provided
-    if (props.onClick) {
-      props.onClick()
-    }
-  }
-  
-  return (
-    <button onClick={handleClick}>
-      {props.label} (clicked {clickCount} times)
-    </button>
-  );
-}
-
 function App() {
-
   // From tutorial, handlers should be in the component themselves
   // const handleClick = () => {
   //   alert('Button clicked');
   // }
-
 
   return (
     <div className="App">
---
+++ src/components/MyButton.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./MyButton.css";
+
+function MyButton(props) {
+  // Initialize teh state variable'clickCount' to 0
+  // NOTE: This is the standard pattern for initializing state in a function component
+
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleClick = () => {
+    // Increment click count
+    setClickCount(clickCount + 1);
+
+    // Call the onClick prop if it is provided
+    if (props.onClick) {
+      props.onClick();
+    }
+  };
+
+  return (
+    <button onClick={handleClick}>
+      {props.label} (clicked {clickCount} times)
+    </button>
+  );
+}
